70ef5ed12e52f0b27c1a8cfcb8089be9a2268ade
Urvashi Biswal <urvashibiswal326@gmail.com>
2020-02-26 20:33:37 -0500

    stock crawler ver 0

--- src/main/java/com/ubiswal/crawlers/main/Main.java
+++ src/main/java/com/ubiswal/crawlers/main/Main.java
@@ -1,12 +1,16 @@
 package com.ubiswal.crawlers.main;
 import com.ubiswal.crawlers.stockprice.StockPriceCrawler;
+import java.io.IOException;
 import java.util.ArrayList;
+import org.apache.http.HttpException;
+
 
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws HttpException {
         System.out.println("Hello world!");
         ArrayList <String> symbols = new ArrayList<>();
         symbols.add("INTC");
         StockPriceCrawler s = new StockPriceCrawler(symbols);
+        s.sendGet();
     }
 }
--- src/main/java/com/ubiswal/crawlers/stockprice/StockPriceCrawler.java
+++ src/main/java/com/ubiswal/crawlers/stockprice/StockPriceCrawler.java
@@ -1,21 +1,55 @@
 package com.ubiswal.crawlers.stockprice;
+
 import java.util.List;
+
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.S3Object;
 
-public class StockPriceCrawler {
-    public StockPriceCrawler(List <String> stockSymbols){
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+import java.net.*;
+import java.io.*;
 
+import org.apache.http.HttpException;
+
+public class StockPriceCrawler {
+    public StockPriceCrawler(List<String> stockSymbols) {
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
         List<Bucket> buckets = s3.listBuckets();
         System.out.println("Your Amazon S3 buckets are:");
         for (Bucket b : buckets) {
             System.out.println("* " + b.getName());
         }
-        S3Object stringObject = new S3Object("HelloWorld.txt", "Hello World!");
-        s3.putObject("stocks_testing", stringObject);
+    }
+
+    public String sendGet() throws HttpException {
+
+        URL urlForGetRequest = null;
+        try {
+            urlForGetRequest = new URL("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=INTC&interval=5min&apikey=2Q4MMOXUEBV2E9XY");
+
+            String readLine = null;
+            HttpURLConnection connection = null;
+            connection = (HttpURLConnection) urlForGetRequest.openConnection();
+            connection.setRequestMethod("GET");
+            //connection.setRequestProperty("userId", "a1bcdef"); // set userId its a sample here
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+                StringBuffer response = new StringBuffer();
+                while ((readLine = in.readLine()) != null) {
+                    response.append(readLine);
+                }
+                in.close();
+                return response.toString();
+
+            } else {
+                throw new HttpException("GET REQUEST DID NOT WORK!! Got a response code of " + responseCode);
+            }
+        } catch (IOException e) {
+            throw new HttpException("GET REQUEST DID NOT WORK!! " + e.getMessage());
+        }
     }
 }
